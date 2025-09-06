50968137492f769bdb1611fe548c65eef53af5be
vvvyyynet <levyn.buerki@unibe.ch>
2025-09-06 18:23:17 +0200

    fix: new deploy config

--- svelte.config.js
+++ svelte.config.js
@@ -17,12 +17,11 @@ const config = {
 	],
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		})
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	},
 	extensions: ['.svelte', '.svx', '.md']
 };
